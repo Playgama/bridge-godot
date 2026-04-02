8e66bee03fb344653e3172bffb1da6798b685e39
Sergei <sergei@playgama.com>
2026-04-02 12:38:20 +0300

    Add advanced banners

--- addons/playgama_bridge/modules/advertisement/advertisement.gd
+++ addons/playgama_bridge/modules/advertisement/advertisement.gd
@@ -1,6 +1,7 @@
 signal banner_state_changed
 signal interstitial_state_changed
 signal rewarded_state_changed
+signal advanced_banners_state_changed
 
 var minimum_delay_between_interstitial setget , _minimum_delay_between_interstitial_getter
 var is_banner_supported setget , _is_banner_supported_getter
@@ -10,6 +11,8 @@ var interstitial_state setget , _interstitial_state_getter
 var is_rewarded_supported setget , _is_rewarded_supported_getter
 var rewarded_state setget , _rewarded_state_getter
 var rewarded_placement setget , _rewarded_placement_getter
+var is_advanced_banners_supported setget , _is_advanced_banners_supported_getter
+var advanced_banners_state setget , _advanced_banners_state_getter
 
 
 func _minimum_delay_between_interstitial_getter():
@@ -36,10 +39,17 @@ func _rewarded_state_getter():
 func _rewarded_placement_getter():
 	return _js_advertisement.rewardedPlacement
 
+func _is_advanced_banners_supported_getter():
+	return _js_advertisement.isAdvancedBannersSupported
+
+func _advanced_banners_state_getter():
+	return _js_advertisement.advancedBannersState
+
 var _js_advertisement = null
 var _js_on_banner_state_changed = JavaScript.create_callback(self, "_on_banner_state_changed")
 var _js_on_interstitial_state_changed = JavaScript.create_callback(self, "_on_interstitial_state_changed")
 var _js_on_rewarded_state_changed = JavaScript.create_callback(self, "_on_rewarded_state_changed")
+var _js_on_advanced_banners_state_changed = JavaScript.create_callback(self, "_on_advanced_banners_state_changed")
 var _utils = load("res://addons/playgama_bridge/utils.gd").new()
 var _check_adblock_callback = null
 var _js_check_adblock_then = JavaScript.create_callback(self, "_on_js_check_adblock_then")
@@ -61,6 +71,12 @@ func show_interstitial(placement = null):
 func show_rewarded(placement = null):
 	_js_advertisement.showRewarded(placement)
 
+func show_advanced_banners(placement = null):
+	_js_advertisement.showAdvancedBanners(placement)
+
+func hide_advanced_banners():
+	_js_advertisement.hideAdvancedBanners()
+
 func check_adblock(callback):
 	if _check_adblock_callback != null:
 		return
@@ -77,6 +93,7 @@ func _init(js_advertisement):
 	_js_advertisement.on('banner_state_changed', _js_on_banner_state_changed)
 	_js_advertisement.on('interstitial_state_changed', _js_on_interstitial_state_changed)
 	_js_advertisement.on('rewarded_state_changed', _js_on_rewarded_state_changed)
+	_js_advertisement.on('advanced_banners_state_changed', _js_on_advanced_banners_state_changed)
 
 func _on_banner_state_changed(args):
 	emit_signal("banner_state_changed", args[0])
@@ -86,6 +103,9 @@ func _on_interstitial_state_changed(args):
 
 func _on_rewarded_state_changed(args):
 	emit_signal("rewarded_state_changed", args[0])
+
+func _on_advanced_banners_state_changed(args):
+	emit_signal("advanced_banners_state_changed", args[0])
 
 func _on_js_check_adblock_then(args):
 	if _check_adblock_callback != null:
